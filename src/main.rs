b454524e7e91a9008a330b9372f5cd8f2ccff3e8
dmangames <talktodmangames@gmail.com>
2023-10-10 00:03:44 -0500

    can call a endpoint and get a static response

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #[macro_use] extern crate rocket;
 use rocket_dyn_templates::{Template, context};
 use rocket::fs::{FileServer, relative};
+use rocket::serde::json::{json, Value};
+use rocket::{http::Status};
 
 #[get("/")]
 fn index() -> Template {
@@ -9,10 +11,20 @@ fn index() -> Template {
     })
 }
 
+#[get("/hello")]
+fn hello() -> Result<Value, Status> {
+  Ok(json!({
+    "key": "value",
+    "array": [1, 2, 3, 4]
+  }))
+}
+
+
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .attach(Template::fairing())
         .mount("/", routes![index])
         .mount("/public", FileServer::from(relative!("static")))
+        .mount("/api", routes![hello])
 }
