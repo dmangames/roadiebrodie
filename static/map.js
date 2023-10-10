d639f4edeb816848d4b2ca875696d3b48aa99f7f
Brandon Choy <91912156+bchoy11@users.noreply.github.com>
2023-10-09 23:55:52 -0500

    removed launch.json from previous commit and added to gitignore

--- static/map.js
+++ static/map.js
@@ -1,32 +1,30 @@
 let map;
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: new google.maps.LatLng(-33.91722, 151.23064),
-    zoom: 16,
-  });
+	map = new google.maps.Map(document.getElementById("map"), {
+	center: new google.maps.LatLng(-33.91722, 151.23064),
+	zoom: 16,
+	});
 
 	const iconBase = 'public/';
 
-  const icons = {
-    yellow_pin: {
-      icon: iconBase + "rb_pin.png",
-    },
-  };
+	const icons = {
+	yellow_pin: {
+		icon: iconBase + "rb_pin.png",
+	},
+	};
 
-google.maps.event.addListener(map, 'click', function(event){
-	placeMarker(event.latLng);
-});
-
-function placeMarker(location) {
-	const marker = new google.maps.Marker({
-	  position: location,
-	  icon: icons["yellow_pin"].icon,
-	  map: map,
+	google.maps.event.addListener(map, 'click', function(event){
+		placeMarker(event.latLng);
 	});
-}
 
+	function placeMarker(location) {
+		const marker = new google.maps.Marker({
+		position: location,
+		icon: icons["yellow_pin"].icon,
+		map: map,
+		});
+	}
 }
-
 
 window.initMap = initMap;
